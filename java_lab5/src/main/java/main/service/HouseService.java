998918e6be69ee5328afc5e965fbd4c531c19154
Advent390 <vladikfeshuk@gmail.com>
2025-03-23 18:24:24 +0200

    Update HouseService.java

--- java_lab5/src/main/java/main/service/HouseService.java
+++ java_lab5/src/main/java/main/service/HouseService.java
@@ -9,40 +9,7 @@ public class HouseService {
     private final HouseRepository repository = new HouseRepositoryTextImpl();
     private final HouseRepository repository1 = new HouseRepositoryBinaryImpl();
 
-//    public void outputArrayTxt(House[] houses, File file) {
-//        repository.outputArrayTxt(houses, file);
-//    }
-//
-//    public void outputArrayTxt(House[] houses, String fileName) {
-//        repository.outputArrayTxt(houses, fileName);
-//    }
-//
-//    public House[] readArrayTxt(File file) {
-//        return repository.readArrayTxt(file);
-//    }
-//
-//    public House[] readArrayTxt(String fileName) {
-//        return repository.readArrayTxt(fileName);
-//    }
-//
-//    public void outputArrayBin(House[] houses, File file) {
-//        repository1.outputArrayBin(houses, file);
-//    }
-//
-//    public void outputArrayBin(House[] houses, String fileName) {
-//        repository1.outputArrayBin(houses, fileName);
-//    }
-//
-//    public House[] readArrayBin(File file) {
-//        return repository1.readArrayBin(file);
-//    }
-//
-//    public House[] readArrayBin(String fileName) {
-//        return repository1.readArrayBin(fileName);
-//    }
-
-
-        public void outputArrayTxt(House[] houses, String fileName) {
+    public void outputArrayTxt(House[] houses, String fileName) {
         repository.outputArray(houses, fileName);
     }
 
